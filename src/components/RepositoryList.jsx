965ca2d3dc6fcdac3231e7a4b6a855c989df081f
Lucas Polizeli <lumpolizeli@gmail.com>
2021-09-21 13:35:49 -0300

    feat: finish repositories list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,7 @@
-import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
+
+import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
 
 const repository = {
   name: "unform",
@@ -8,12 +10,22 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
-    <section className="respository-list">
+    <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem repository={repository} />
+        ))}
       </ul>
     </section>
   );
